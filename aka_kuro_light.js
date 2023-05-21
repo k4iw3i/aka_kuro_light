d170f06a7857adde7604dda9fec4cc8e5d7fa7e6
Kai Wei <kai.f.wei@gmail.com>
2023-05-21 16:23:46 +0000

    files added;

---
+++ aka_kuro_light.js
@@ -0,0 +1,26 @@
+black       = '#29343D';
+red         = '#F8961E'; // red
+green       = '#90BE6D'; // green
+yellow      = '#DBC18E'; // yellow
+blue        = '#577590'; // blue
+magenta     = '#F9C74F'; // pink
+cyan        = '#4CA24A'; // cyan
+white       = '#555555'; // light gray
+lightBlack  = '#394956'; // medium gray
+lightRed    = '#F8961E'; // red
+lightGreen  = '#90BE6D'; // green
+lightYellow = '#F3722C'; // yellow
+lightBlue   = '#577590'; // blue
+lightMagenta= '#F9c74F'; // pink
+lightCyan   = '#43AA8B'; // cyan
+lightWhite  = '#ABABAB'; // white
+ 
+t.prefs_.set('color-palette-overrides',
+                 [ black , red     , green  , yellow,
+                  blue     , magenta , cyan   , white,
+                  lightBlack   , lightRed  , lightGreen , lightYellow,
+                  lightBlue    , lightMagenta  , lightCyan  , lightWhite ]);
+ 
+t.prefs_.set('cursor-color', '#4C4C4C');
+t.prefs_.set('foreground-color', '#29343D');
+t.prefs_.set('background-color', '#FFFFFF');
